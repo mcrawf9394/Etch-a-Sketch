765689b64c5e177e79ecd5c869b50a27155c6beb
mcrawf9394 <mjc9394@gmail.com>
2024-02-27 20:46:01 -0500

    The user input from the form is being sent to the function but the content that is added to the html disappears immediately. Not exactly sure what is happening yet.

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 const form = document.getElementById('form');
+let count
+const etchASketchGrid = document.querySelector('.etchASketchGrid')
 form.addEventListener('submit', function (userEntry){
     const gridCount = document.getElementById('gridCount').value
-    if (gridCount > 100) {
-        userEntry.preventDefault()
-        alert("Can not enter a value higher than 100.")
-    }
-    else if (gridCount > 0){
-        var count = parseInt(gridCount)
-        changingGridCount (count)
-    }
-    else{
+    if (gridCount = " "){
         userEntry.preventDefault
-        alert("Value can not be 0")
+    }
+    count = parseInt(gridCount)
+    if (count > 100 || count <= 0) {
+        userEntry.preventDefault()
+        alert("Can not enter a value higher than 100 or less than or equal to 0.")
+    }
+    else if (count > 0){
+        changingGridCount(count)
     }
 })
 function changingGridCount (){
-    const etchASketchGrid = document.querySelector('.etchASketchGrid')
-    let i = 0
-    for (i < gridCount; i++;) {
+    for (let i = 0; i < count; i++) {
         const individualGrids = document.createElement("div");
         individualGrids.classList.add('individualGrids');
         individualGrids.id = "grid" + i;
