dedfd495b23d3e33739679813e07e6fc71d7d50a
mcrawf9394 <mjc9394@gmail.com>
2024-02-29 11:30:12 -0500

    Creating grids with input now, need to make it to where it only allows one input and the reset button clears the grid.

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const container = document.getElementById('etchASketchGrid')
 const gridRow = document.createElement('div')
 const gridColoumn = document.createElement('div')
+const form = document.getElementById('form')
+let count
+form.addEventListener('submit', function (userEntry){
+    const gridCount = document.getElementById('gridCount').value
+    count = parseInt(gridCount)
+    getUserNumber (count)
+    userEntry.preventDefault()
+})
+form.addEventListener('reset', function (){
+    
+})
+function getUserNumber (){
+    if (count > 100 || count <= 0) {
+        alert("Can not enter a value higher than 100 or less than or equal to 0.")
+    }
+    else {
+        makeGrid(count)
+    }
+}
 function makeGrid (){
-    for (j = 0; j < 16; j++){
+    for (j = 0; j < count; j++){
         let column = document.createElement('div')
         column.classList.add("gridColumns")
         column.style.backgroundColor = 'blue'
         column.textContent ="this is a test"
         container.appendChild(column)
-        for (i = 0; i < 16; i++){
+        for (i = 0; i < count; i++){
             let row = document.createElement('div')
             row.classList.add('gridRows')
             row.style.backgroundColor = 'blue'
@@ -17,4 +36,3 @@ function makeGrid (){
         }
     }
 }
-makeGrid ()
